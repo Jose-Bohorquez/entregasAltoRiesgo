e5802482425cc31ce34bd43f275ff1fc43a29389
Jose Bohorquez <bd567358546@gmail.com>
2024-08-09 03:57:57 -0500

    comentando linea a linea en lo que me adapto ahora config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-# config.py
-
+# Importar el módulo os para interactuar con el sistema operativo
 import os
 
+# Definir una clase para almacenar la configuración de la aplicación.
 class Config:
+    # La clave secreta utilizada para la seguridad, como firmar cookies.
+    # Se obtiene del entorno si está disponible; de lo contrario, se usa un valor por defecto.
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    
+    # La URI de la base de datos, que especifica dónde se encuentra la base de datos.
+    # Se obtiene del entorno si está disponible; de lo contrario, se usa una base de datos SQLite por defecto.
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
+    
+    # Desactivar el seguimiento de modificaciones en la base de datos para evitar advertencias.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
