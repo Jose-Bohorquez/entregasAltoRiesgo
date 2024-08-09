e861aa8ff7c4d0b787dee82fde23d944d057e750
Jose Bohorquez <bd567358546@gmail.com>
2024-08-09 04:02:03 -0500

    comentando linea a linea en lo que me adapto ahora controllers/user_controller.py

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,9 +1,11 @@
-# controllers/user_controller.py
-
+# Importar las funciones necesarias de Flask y la clase User del modelo.
 from flask import render_template
 from models.user_model import User
 
+# Definir la función get_user_info para manejar la lógica relacionada con los usuarios.
 def get_user_info():
-    # Crear un usuario de ejemplo
+    # Crear un usuario de ejemplo.
     user = User(id=1, name="John Doe", email="john@example.com")
+    
+    # Renderizar la plantilla 'user.html' y pasarle el objeto 'user'.
     return render_template('user.html', user=user)
